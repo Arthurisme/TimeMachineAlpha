c9bcef190522f1d0bdf782fd8b6dc5b6b3058958
Arthurisme <is@live.ca>
2016-08-08 12:29:41 -0400

    Change eclipse to Intellij

--- Backend/SpringBoot/Eclipse/backend/src/main/java/com/sensoryex/backend/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.sensoryex.backend.service;
-
-import com.sensoryex.backend.model.User;
-
-public interface UserService {
-
-	User save(User user);
-	User findByUserName( String userName);
-}
--- Frontend/build/models/photo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Photo = (function () {
-    function Photo() {
-    }
-    return Photo;
-}());
-exports.Photo = Photo;
-//# sourceMappingURL=photo.js.map
